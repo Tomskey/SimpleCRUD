753bc8bfd0547f7daebe110ec68cc78b65828437
Tomasz <tomasz.sokolowski.t@gmail.com>
2018-06-23 19:30:07 +0200

    ~

--- src/main/java/com/example/TSNotes/TSNotes/model/dto/NoteDto.java
+++ src/main/java/com/example/TSNotes/TSNotes/model/dto/NoteDto.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
 @Setter
 @Getter
 public class NoteDto {
@@ -14,5 +16,9 @@ public class NoteDto {
     private String content;
     private String createdAt;
     private String updatedAt;
-    private String tag;
+    private Set<String> tags;
+
+    public void addTag(String tag){
+        tags.add(tag);
+    }
 }
